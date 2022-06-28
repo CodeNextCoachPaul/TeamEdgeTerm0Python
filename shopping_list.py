7f47b4542438f03359da47d561d610ddf8f53c68
CodeNextCoachPaul <108353724+CodeNextCoachPaul@users.noreply.github.com>
2022-06-28 11:38:16 -0400

    Update shopping_list.py

--- shopping_list.py
+++ shopping_list.py
@@ -23,11 +23,11 @@ print(welcome_message)
 
 #-->Todo: declare a shopping_list list
 
+shopping_list = input("Enter a shopping list")
 
 def prompt_user():
 
     reply = input("What do you want to add or remove?  >>  ")
-
     return reply
 
 def check_answer(ans):
@@ -35,11 +35,13 @@ def check_answer(ans):
 
 
 def add_item():
+    shopping_list.append(reply)
 #this function can take in a string and store it in an array
     pass
 
 
 def remove_item():
+    shopping_list.pop()
     pass
 
 while active:
