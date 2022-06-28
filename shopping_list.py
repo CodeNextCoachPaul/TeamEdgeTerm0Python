54b4381533d0b95500f937fbac9001554e548ddb
CodeNextCoachPaul <108353724+CodeNextCoachPaul@users.noreply.github.com>
2022-06-28 11:42:39 -0400

    Update shopping_list.py

--- shopping_list.py
+++ shopping_list.py
@@ -1,33 +1,10 @@
-#********************************************************************
- #                                                                 
- #  Team Edge List Mini-project: THE SHOPPING LIST HELPER
- # 
- #  This project prompts users using input() to prompt users
- #  to add (or remove) items from a shopping list. It starts empty
- #  and each time the program is run it asks you to either add or 
- #  remove an item from the list. It also updates the user of its
- #  contents. The shopping list also checks to see if an item 
- #  is already present in the list and prevents you from adding it
- #  again, giving feedback along the way. 
- # 
- # ***************************************************************/
-
-active = True
-
-print("Welcome to Shopping List!")
-
-welcome_message = "Hi! I'm your shopping assistant. Let me take your order. \n You can type 'add milk' to add milk to your shopping list. \n or you can type 'remove milk' to remove it. \n"
-
-print(welcome_message)
-
-
 #-->Todo: declare a shopping_list list
 
+shopping_list = input("Enter a shopping list")
 
 def prompt_user():
 
     reply = input("What do you want to add or remove?  >>  ")
-
     return reply
 
 def check_answer(ans):
@@ -35,13 +12,14 @@ def check_answer(ans):
 
 
 def add_item():
+    shopping_list.append(reply)
 #this function can take in a string and store it in an array
     pass
 
 
 def remove_item():
+    shopping_list.pop()
     pass
 
 while active:
-
-    check_answer(prompt_user()) #this makes the program continously prompt and check response while the boolean 'active' returns True
+	
