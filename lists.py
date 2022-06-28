ff527a972b910aa2069c49656e6875e3c2740dac
CodeNextCoachPaul <108353724+CodeNextCoachPaul@users.noreply.github.com>
2022-06-28 11:41:12 -0400

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,31 +1,3 @@
-#********************************************************************
-                                                                  
- #  Team Edge list Challenges!                                     
-  
- #  Use this program to learn about Python lists:
-    
- #  1. Declare and store data in lists
- #  2. Access list data by index
- #  3. Modify list data by index
- #  4. Multi-dimensional lists (lists inside lists!)
- #  5. List methods: append() and pop()
- #  6. The length of the list - introduce conditionals
- #  7. Strings as sequences - join() split() 
- #  
- #  Follow the -->To Dos ! run the program to test often and debug
- # ***************************************************************
-
-
-
-
-
-print("------------------- CHALLENGE 1 -------------------")
-#This is an empty Python list:
-empty_list = []
-
-#this list has 5 Strings. We can print it:
-names = ["Julian", "Wolf", "Alex", "Steph", "Alessandro"]
-print("names: " + str(names))  
 
 #-->TODO: Declare another list called friends with at least 5 strings inside (if you don't have 5 friends make them up!)
 
@@ -36,6 +8,7 @@ numbers = [12.9, 23.4 , 100, 3.1415 , 500, 1.20]
 print("numbers: " + str(numbers)) 
 
 #-->TODO: Declare another list and add in at least 5 numbers. Why five? I don't know. It just feels right.
+
 
 numbers = [19, 24 , 10, 3.3 , 5, 1.2]
 
@@ -53,18 +26,19 @@ newList = ["pizza",5,5.01, False,["hi","bye"]
 newList = ["pizza",5,5.01, False,["hi","bye"]
 
 print("------------------- CHALLENGE 2 -------------------")
- 
-#This code logs the first element of the names list:
-print("The first name is " + names[0])
+
+@@ -55,17 +59,20 @@
 
 #-->TODO: Print the name of your best friend from your friends list
 
+
 print(friends[1])
-           
+
 #-->TODO: Print the first AND last elements of any list you made, or make a brand new one.
 
+
 print(friends[0],friends[4])
-           
+
 print("------------------- CHALLENGE 3 -------------------")
 #this code changes the value of the second element of the names list, then we print the list:
 names[1] = "Alyssa"
@@ -76,11 +50,7 @@ friends[2] = "NewName"
 
 #The code below uses the times_ten() function to multiply the first element in our list by 10:
 def times_ten(number):
-    number = number * 10
-    return number
-
-numbers[0] = times_ten(numbers[0])
-print(numbers)
+@@ -77,7 +84,12 @@ def times_ten(number):
 
 #-->TODO: Write another function that multiplies a number by 1000 and print the list, as above 
 
@@ -93,33 +63,27 @@ print(numbers)
 
 #-->TODO: Replace your random list elements with anything you want, using the index. 
 
-print("------------------- CHALLENGE 4 -------------------")
-
-#As it turns out, you can also store lists within lists! Declare them and store them as variables.
-child_list_1 = ["This", "is" , "a", "meaningless", "list"]
-child_list_2 = ["This" ,"list" , "is", "also" , "meaningless"]
-parent_list = [child_list_1, child_list_2]
-print("This list has babies: " + str(parent_list))
+@@ -91,7 +103,7 @@ def times_ten(number):
 
 #-->TODO: Store and print all the lists we have worked on thus far in a new parent list
+
 
 parentList = [friends,child_list_1, child_list_2,numbers]
 print("------------------- CHALLENGE 5 -------------------")
 
 #We can add elements into a list wihtout replacing anything. Using append() adds an element to the END of the list:
-movies = ["Toy Story 4", "The Dark Knight", "Parasite"]
-print("Movies: " + str(movies))
-movies.append("Joker")
-movies.append("Black Panther")
+@@ -102,17 +114,20 @@ def times_ten(number):
 print("Movies now has: " + str(movies))
 
 #-->TODO: Declare a list with 5 favorite songs
+
 newItems = ["I Need A Girl Pt2","Ether","6 Foot 7 Foot","Show Me What You Got","Hot in Here"]
-  
+
 #-->TODO: Add 2-3 more songs using .append() and print both before and after.
+
 newItemsTwo = ["TRAGIC","Bad Idea","Foolish"]
 newItems.append(newItemsTwo)
-          
+
 #We can also remove elements using .pop(), which removes the last element or the element at the given index. You can store it after it comes out:
 cities = ["New York", "Oakland", "Las Vegas", "Topeka"]
 print("cities: " + str(cities))
@@ -127,51 +91,41 @@ unwanted_city = cities.pop()
 print("unwanted city: " + str(unwanted_city))
 
 #-->TODO: remove your last song using .pop() and print the removed element as above
+
 newItemsTwo.pop()
 #Note: there are more methods to remove and modify list elements. We will cover those later
 
 print("------------------- CHALLENGE 6 -------------------")
-
-#Lists have properties. one of the most important is the size, or length. Here are two examples:
-print(f"I have {len(names)} friends")
-
-how_many_cities = len(cities)
-print(f"There are {how_many_cities} ciites in my list")
+@@ -125,7 +140,7 @@ def times_ten(number):
 
 #-->TODO: Print out the number of friends, or other items from other lists using string literals as above
+
 
 print(f"Number of friends is {len(friends)}")
 #The len() function is key, especially in conditionals or to simply count how many times to do something.
 
 if len(numbers) > 3:
-    print("There are more than 3 numbers in my list")
-else:
+@@ -134,7 +149,9 @@ def times_ten(number):
     print("I need more numbers in my list!!!")
 
 #-->TODO: Write another if/else statement to check the size of your songs list. If you have 5 of less, add two more songs!
+
 if len(newItemsTwo) < 5:
            tup = ("Work It", "One Two Step")
            newItemsTwo.append(tup)
 
 print("------------------- CHALLENGE 6 -------------------")
 
-#Strings can also be thought of lists:
-sentence = "I am a boring sentence."
-boring_list = sentence.split(" ") #split the sentence by each space to get a list of words
-print("boring sentence as a list: " + str(boring_list))
-
-word = "Abracadabra"
-word_split_list = word.split() #split by nothing, and you get each character in the string as its own element.
-print("letter by letter: " + str(word_split_list))
-
-#using this you can split strings up by any character!
+@@ -151,12 +168,15 @@ def times_ten(number):
 
 #-->TODO: Change the name of the person who is late in this sentence and print it.
 split_me = "I heard Alex was late to class today."
+
 splitList = split_me.split(" ")
 splitList[2] = "Mike"
 #-->TODO: Add an exclamation mark to this sentence using split() and append(), then print. (yes, there are other ways, but...)
 make_me_exciting = "What a wonderful day"
+
 splitSent = make_me_exciting.split(" ")
 splitSent.append("!")
 #We can also join our list elements into a string using.....join()!
